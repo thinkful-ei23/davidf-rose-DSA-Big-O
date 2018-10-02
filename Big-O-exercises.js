3f3bb98901c50a5a8ef97edd94297389107b7bb7
David Folks <aldwyn.wot@gmail.com>
2018-10-02 09:29:18 -0600

    Doubler

--- Big-O-exercises.js
+++ Big-O-exercises.js
@@ -10,12 +10,11 @@ function isEven(value) {
 
 //Overall EVEN OR ODD has O(1) or constant complexity
 
-
 //ARE YOU HERE
 function areYouHere(arr1, arr2) {
-  for (let i=0; i<arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     const el1 = arr1[i];
-    for (let j=0; j<arr2.length; j++) {
+    for (let j = 0; j < arr2.length; j++) {
       const el2 = arr2[j];
       if (el1 === el2) return true;
     }
@@ -23,4 +22,15 @@ function areYouHere(arr1, arr2) {
   return false;
 }
 //O(n^2)
-//Polynomial aka quadratic time 
+//Polynomial aka quadratic time
+
+//DOUBLER
+
+function doubleArrayValues(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] *= 2; //O(n)
+  }
+  return array;
+}
+
+//O(n) or Linear complexity

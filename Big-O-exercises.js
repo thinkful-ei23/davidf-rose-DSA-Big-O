e982a5fbd43f1eaa45bf5977572d1cd5686b1b67
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-02 10:26:17 -0500

    are you here

--- Big-O-exercises.js
+++ Big-O-exercises.js
@@ -9,3 +9,18 @@ function isEven(value) {
 }
 
 //Overall EVEN OR ODD has O(1) or constant complexity
+
+
+//ARE YOU HERE
+function areYouHere(arr1, arr2) {
+  for (let i=0; i<arr1.length; i++) {
+    const el1 = arr1[i];
+    for (let j=0; j<arr2.length; j++) {
+      const el2 = arr2[j];
+      if (el1 === el2) return true;
+    }
+  }
+  return false;
+}
+//O(n^2)
+//Polynomial aka quadratic time 

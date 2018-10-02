eff6e1ffd34489078fa218d3882d33b8125cdf1b
David Folks <aldwyn.wot@gmail.com>
2018-10-02 09:22:52 -0600

    Even or Odd

---
+++ Big-O-exercises.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//EVEN OR ODD
+
+function isEven(value) {
+  if (value % 2 == 0) {
+    return true; //O(1)
+  } else return false; //O(1)
+}
+
+//Overall EVEN OR ODD has O(1) or constant complexity

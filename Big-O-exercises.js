b273d29fd594d8a004584ec3593301450351e9b7
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-02 10:42:47 -0500

    fibonacci

--- Big-O-exercises.js
+++ Big-O-exercises.js
@@ -56,3 +56,32 @@ function createPairs(arr) {
 }
 
 //O(n^2) or polynomial complexity
+
+
+//FIBONACCI ITERATIVE VERSION 
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+//O(n) linear time
